73e6047c1bd3bc4b4b8f78c548694e893f7a822b
Justin Maillet <justy777@icloud.com>
2025-04-23 17:07:18 -0300

    Minor improvements

--- app/page.js
+++ app/page.js
@@ -1,17 +1,6 @@
 import SortableTable from "../components/sortable-table";
 
-const movies = [
-  { id: 1, title: "American Psycho", year: 2000 },
-  { id: 2, title: "The Thing", year: 1982 },
-  { id: 3, title: "Jaws", year: 1975 },
-  { id: 4, title: "Heat", year: 1995 },
-  { id: 5, title: "Top Gun", year: 1986 },
-  { id: 6, title: "Pulp Fiction", year: 1994 },
-  { id: 7, title: "The Lost Boys", year: 1987 },
-  { id: 8, title: "Ed Wood", year: 1994 },
-  { id: 9, title: "The Game", year: 1997 },
-  { id: 10, title: "Some Like It Hot", year: 1959 },
-];
+import { movies } from "../lib/movie-data";
 
 export default function Page() {
   return (
